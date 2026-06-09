0b830ade5a92d6daa4059f2051a843a1c1811fa1
Rafal Kuszczuk <rafalkuszczuk01@gmail.com>
2026-06-10 01:19:12 +0200

    Implementacja mechaniki strzelania, niszczenia celow i animacji implozji

--- Target.h
+++ Target.h
@@ -6,12 +6,22 @@ using namespace std;
 class Target {
 protected:
     float x, y, radius;
+    bool shrinking = false; // Flaga wyzwalająca animację
+    bool dead = false;      // Flaga oznaczająca koniec animacji (do usunięcia)
 
 public:
     Target(float startX, float startY, float r) : x(startX), y(startY), radius(r) {}
 
-    virtual ~Target() = default; // Wirtualny destruktor to podstawa polimorfizmu
+    virtual ~Target() = default;
 
     virtual void update(float deltaTime) = 0;
     virtual void draw(sf::RenderWindow& window) = 0;
+
+    float getX() const { return x; }
+    float getY() const { return y; }
+    float getRadius() const { return radius; }
+
+    // Metody do obsługi animacji i usuwania
+    void startShrinking() { shrinking = true; }
+    bool isDead() const { return dead; }
 };
